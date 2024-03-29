3bffd7c14b8596333d071892cb64f916acefc8e5
Dmitry Fedorov <95097887+fedorovvdmitry@users.noreply.github.com>
2024-03-29 12:38:04 +0300

    UPD popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,16 +4,5 @@ document.addEventListener('DOMContentLoaded', () => {
     tooltip.addEventListener('click', () => {
       tooltip.children[0].classList.toggle('show')
     })
-  })
-
-  const popup_about = document.querySelectorAll('.button_about')
-    popup_about.addEventListener('click', () => {
-      popup_about.classList.toggle('show')
-    })
-
-    // function openPopupAbout () {
-    //   let popup_about = document.querySelectorAll('.popup_about');
-    //   popup_about.classList.toggle("show");
-    // }
-  
+  })  
 })
